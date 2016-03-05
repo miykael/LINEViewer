842243e091e169782c4b4c3c6dba4ec529191851
miykael <michaelnotter@hotmail.com>
2016-03-05 19:41:59 +0100

    FIX: updates for PyPI submission

---
+++ lineviewer/__init__.py
@@ -0,0 +1,7 @@
+####################################################################
+# Python based EEG analysis tool to give you a rough data overview #
+####################################################################
+
+__version__ = '0.1.0'
+__package__ = 'lineviewer'
+__url__ = 'https://github.com/miykael/LINEViewer'
