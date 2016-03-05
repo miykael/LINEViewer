c7872f79554259c6b13e357fda182ffb0b991e68
miykael <michaelnotter@hotmail.com>
2016-03-05 18:16:06 +0100

    FIX: preparation for PyPI upload

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
-setup(
-    name='lineviewer',
-    packages=['lineviewer'],
-    version='0.1.0',
-    description='Python based EEG analysis tool for a rough data overview',
-    long_description="""Python based EEG analysis tool to give you a rough
-    data overview""",
-    license='GNU GENERAL PUBLIC LICENSE',
-    author='Michael Notter',
-    author_email='michaelnotter@hotmail.com',
-    url='https://github.com/miykael/LINEViewer',
-    download_url='https://github.com/miykael/LINEViewer/tarball/0.1',
-    keywords=['LINE', 'LINEViewer', 'EEG'],
-    classifiers=[],
-)
+if __name__ == "__main__":
+    setup(
+        name='lineviewer',
+        packages=['lineviewer'],
+        version='0.1.0',
+        description='Python based EEG analysis tool for a rough data overview',
+        long_description="""Python based EEG analysis tool to give you a rough
+        data overview""",
+        license='GNU GENERAL PUBLIC LICENSE',
+        author='Michael Notter',
+        author_email='michaelnotter@hotmail.com',
+        url='https://github.com/miykael/LINEViewer',
+        download_url='https://github.com/miykael/LINEViewer/tarball/0.1',
+        keywords=['LINE', 'LINEViewer', 'EEG'],
+    )
