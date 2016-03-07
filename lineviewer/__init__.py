7d0ea55664c2495b47827bfd283a2ac17908140c
miykael <michaelnotter@hotmail.com>
2016-03-07 22:18:45 +0100

    ENH: Release 0.1.6 update

--- lineviewer/__init__.py
+++ lineviewer/__init__.py
@@ -5,7 +5,7 @@
 from LINEViewer import StartLINEViewer
 
 # version info for LINEViewer
-__version__ = '0.1.4'
+__version__ = '0.1.6'
 __package__ = 'lineviewer'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Michael Notter'
